a7fb3760eaf056450ea521c5eadddc06483b5338
onehao111@gmail.com <onehao111@gmail.com@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-05-12 04:01:11 +0000

    Complete - task : 21_事务的保存点处理

--- trunk/jdbc/src/cn/ibm/onehao/SavePointTest.java
+++ trunk/jdbc/src/cn/ibm/onehao/SavePointTest.java
@@ -6,6 +6,7 @@ package cn.ibm.onehao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Statement;
 
 /**
@@ -27,11 +28,17 @@ public class SavePointTest {
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
+		Savepoint sp = null;
 		try {
 			conn = JdbcUtils.getConnection();
 			conn.setAutoCommit(false);//事务起点  mysql engine 是 InnoDB的时候是支持事务的
 			st = conn.createStatement();
+			
 			String sql = "update user set money=money-10 where id=15";
+			st.executeUpdate(sql);
+			sp = conn.setSavepoint();
+			
+			sql = "update user set money=money-10 where id=12";
 			st.executeUpdate(sql);
 			
 			sql = "select money from user where id=13";
@@ -45,7 +52,13 @@ public class SavePointTest {
 			sql = "update user set money=money+10 where id=13";
 			st.executeUpdate(sql);
 			conn.commit();//事务终点
-		} catch(SQLException e){
+		} catch(RuntimeException e){
+			if(conn != null&&sp != null){
+				conn.rollback(sp);
+			conn.commit();
+			}
+			throw e;
+		}catch(SQLException e){
 			if(conn != null)
 				conn.rollback();
 			throw e;
