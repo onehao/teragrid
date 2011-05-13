c7880594c3c3c836dc01a50512fd099474ee067f
onehao111@gmail.com <onehao111@gmail.com@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-05-13 08:01:20 +0000

    Complete - task : 27_可更新和对更新敏感的结果集

--- trunk/jdbc/src/cn/ibm/onehao/OtherApi.java
+++ trunk/jdbc/src/cn/ibm/onehao/OtherApi.java
@@ -18,12 +18,52 @@ public class OtherApi {
 	/**
 	 * @param args
 	 * @throws SQLException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws SQLException {
-		int id = create();
-		System.out.println("id: " + id);
+	public static void main(String[] args) throws SQLException, InterruptedException {
+//		int id = create();//test1
+//		System.out.println("id: " + id);
+//		read();//test2
+		read2();//test3
 	}
 
+	
+	static void read2() throws SQLException, InterruptedException{
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			//2.建立连接
+			conn = JdbcUtils.getConnection(); //推荐 代码不罗嗦
+			//conn = JdbcUtilsSingleton.getInstance().getConnection();//单例模式实现
+			
+			//3.创建语句
+			st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			
+			//4.执行语句
+			rs = st.executeQuery("select id,username,birthday,money from user");
+			
+			//5.处理结果
+			while(rs.next()){
+				int id = rs.getInt("id");//有些数据库调用第二次getXXX 同一字段会报错。
+				System.out.println("show " + id);
+				Thread.sleep(2000);
+				System.out.println("id: " + id + "..." 
+						+ "\tusername: " + rs.getString("username") 
+						+ "\tbirthday: " + rs.getDate("birthday")
+						+ "\tmoney: " + rs.getFloat("money"));
+				String name = rs.getString("username");
+				if("otherapi1".equals(name)){
+					rs.updateFloat("money", 300f);
+					rs.updateRow();
+				}
+			}
+		} finally {
+			JdbcUtils.free(rs, st, conn);
+		}
+	}
+	
+	
 	//创建后返回主键
 	static int create() throws SQLException{
 		Connection conn = null;
@@ -46,6 +86,38 @@ public class OtherApi {
 			return id;
 		} finally {
 			JdbcUtils.free(rs, ps, conn);
+		}
+	}
+	
+	static void read() throws SQLException{
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			//2.建立连接
+			conn = JdbcUtils.getConnection(); //推荐 代码不罗嗦
+			//conn = JdbcUtilsSingleton.getInstance().getConnection();//单例模式实现
+			
+			//3.创建语句
+			st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			
+			//4.执行语句
+			rs = st.executeQuery("select id,username,birthday,money from user");
+			
+			//5.处理结果
+			while(rs.next()){
+				System.out.println("id: "+rs.getInt("id") 
+						+ "\tusername: " + rs.getString("username") 
+						+ "\tbirthday: " + rs.getDate("birthday")
+						+ "\tmoney: " + rs.getFloat("money"));
+				String name = rs.getString("username");
+				if("otherapi1".equals(name)){
+					rs.updateFloat("money", 300f);
+					rs.updateRow();
+				}
+			}
+		} finally {
+			JdbcUtils.free(rs, st, conn);
 		}
 	}
 }
