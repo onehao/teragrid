99c3823bd19aa0098dc727e82b37c830d0b4a4fd
onehao111@gmail.com <onehao111@gmail.com@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-05-20 08:34:41 +0000

    Complete - task : 44_JdbcTemplate类中的其他各个查询方法

--- trunk/jdbc/src/cn/ibm/onehao/spring/JdbcTemplateTest.java
+++ trunk/jdbc/src/cn/ibm/onehao/spring/JdbcTemplateTest.java
@@ -5,6 +5,8 @@ package cn.ibm.onehao.spring;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,17 +20,67 @@ import cn.ibm.onehao.domain.User;
  *
  */
 public class JdbcTemplateTest {
+	
+	static JdbcTemplate jdbc = new JdbcTemplate(JdbcUtils.getDataSource());
 
 	/**
 	 * @param args
+	 * @这个类是线程安全的
 	 */
 	public static void main(String[] args) {
 		User user = findUser("onehao");
 		System.out.println(user);
+		
+		System.out.println("users" + findUsers(20));
+		
+		System.out.println("user count:" + getUserCount());
+		
+		System.out.println("user name:" + getUserName(1));
+		
+		System.out.println("user data:" + getData(1));
+		
+		System.out.println("user datas" + getDatas());
+		
+		
 	}
 	
+	static List<Map<String,Object>> getDatas(){
+		String sql = "select id, username, birthday, money from user where id>0";
+		return jdbc.queryForList(sql);
+	}
+	
+
+	static Map<String,Object> getData(int id){
+		String sql = "select id, username, birthday, money from user where id=" + id;
+		return jdbc.queryForMap(sql);
+	}
+	
+	static String getUserName(int id){
+		String sql = "select username from user where id=" + id;
+		Object name = jdbc.queryForObject(sql, String.class);
+		return (String)name;
+		
+	}
+	
+	static int getUserCount(){
+		String sql = "select count(*) from user";
+		return jdbc.queryForInt(sql);
+		
+	}
+	
+	static List<User> findUsers(int id){
+		
+		String sql = "select id, username, birthday, money from user where id<?";
+		Object[] args = new Object[]{id};
+		@SuppressWarnings("unchecked")
+		
+		//实现累世ORMTest.java
+		List<User> users = jdbc.query(sql, args, new BeanPropertyRowMapper<User>(User.class));
+		return users;
+	}
+	
+	
 	static User findUser(String username){
-		JdbcTemplate jdbc = new JdbcTemplate(JdbcUtils.getDataSource());
 		String sql = "select id, username, birthday, money from user where username=?";
 		Object[] args = new Object[]{username};
 		@SuppressWarnings("unchecked")
@@ -39,11 +91,10 @@ public class JdbcTemplateTest {
 	}
 	
 	static User findUser1(String username){
-		JdbcTemplate jdbc = new JdbcTemplate(JdbcUtils.getDataSource());
 		String sql = "select id, username, birthday, money from user where username=?";
 		Object[] args = new Object[]{username};
 		@SuppressWarnings("unchecked")
-		Object user = jdbc.queryForObject(sql, args, new RowMapper(){
+		Object user = jdbc.queryForObject(sql, args, new RowMapper<Object>(){
 			@Override
 			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
 				User user = new User();
