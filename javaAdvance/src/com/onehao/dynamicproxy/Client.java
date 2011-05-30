22612aa58002e2b87a4dc8287473daac2ea9bd02
onehao111 <onehao111@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-05-30 15:24:14 +0000

    Incomplete - task : Java SE 066 Java动态代理模式详解

---
+++ javaAdvance/src/com/onehao/dynamicproxy/Client.java
@@ -0,0 +1,23 @@
+package com.onehao.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class Client {
+
+	public static void main(String[] args) {
+		RealSubject realSubject = new RealSubject();
+		
+		InvocationHandler handler = new DynamicSubject(realSubject);
+		
+		Class<?> classType = handler.getClass();
+		//下面代码一次性生成代理
+		
+		Subject sub = (Subject)Proxy.newProxyInstance(classType.getClassLoader(),
+		realSubject.getClass().getInterfaces(), handler);
+		
+		sub.request();
+		
+		System.out.println(sub.getClass());
+	}
+}
