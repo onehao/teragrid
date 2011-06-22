ccf468cd455d96e6318e4f03631287f563f4298b
onehao111@gmail.com <onehao111@gmail.com@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-06-22 03:27:52 +0000

    Incomplete - task : Java SE 047 实现自己的Comparator及Collections详解

--- trunk/javaAdvance/src/com/onehao/collection/set/TreeSetTest2.java
+++ trunk/javaAdvance/src/com/onehao/collection/set/TreeSetTest2.java
@@ -7,17 +7,17 @@ public class TreeSetTest2
 {
 	public static void main(String[] args)
 	{
-		TreeSet set = new TreeSet();
+		TreeSet set = new TreeSet(new Examinee());
 		
-		Examinee e1 = new Examinee(10);
+		Examinee e1 = new Examinee(50);
 		Examinee e2 = new Examinee(20);
 		Examinee e3 = new Examinee(30);
 		Examinee e4 = new Examinee(40);
 		
 		set.add(e1);
 		set.add(e2);
-//		set.add(e3);
-//		set.add(e4);
+		set.add(e3);
+		set.add(e4);
 		
 		System.out.println(set);
 	}
@@ -31,6 +31,10 @@ class Examinee implements Comparator
 	{
 		this.score = score;
 	}
+	Examinee()
+	{
+		
+	}
 
 	@Override
 	public String toString()
@@ -41,8 +45,15 @@ class Examinee implements Comparator
 	@Override
 	public int compare(Object o1, Object o2)
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		Examinee e1 = (Examinee)o1;
+		Examinee e2 = (Examinee)o2;
+		
+		if(e1.score > e2.score)
+			return 1;
+		else if(e1.score < e2.score)
+			return -1;
+		else 
+			return 0;
 	}
 	
 	
