29786ff04e2f0258d76bf93d8f3a7c30e9215523
onehao111 <onehao111@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-06-24 15:27:42 +0000

    Complete - task : Java SE 054 自定义泛型与泛型的常见陷阱

--- trunk/javaAdvance/src/com/onehao/jdk5/generics/ArrayListTest.java
+++ trunk/javaAdvance/src/com/onehao/jdk5/generics/ArrayListTest.java
@@ -1,22 +1,41 @@
 package com.onehao.jdk5.generics;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ArrayListTest
 {
 	public static void main(String[] args)
 	{
-		List list = new ArrayList();
+		List<String> list = new ArrayList<String>();
 		
-		list.add("String");
-		list.add(new Integer(2));
-		list.add(new Boolean(false));
+//		List list = new ArrayList();
+//		list.add("String");
+//		list.add(new Integer(2));
+//		list.add(new Boolean(false));
+//		
+//		String str = (String)list.get(0);
+//		Integer in = (Integer)list.get(1);
+//		String b = (String)list.get(2); //运行时抛出异常
+//		
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
 		
-		String str = (String)list.get(0);
-		Integer in = (Integer)list.get(1);
-		String b = (String)list.get(2); //运行时抛出异常
+		for(int i = 0; i < list.size(); i++)
+		{
+			String value = list.get(i);
+			System.out.println(value);
+		}
 		
+		System.out.println(" ============");
 		
+		for(Iterator<String> iter = list.iterator();iter.hasNext();)
+		{
+			String value = iter.next();
+			System.out.println(value);
+		}
 	}
 }
