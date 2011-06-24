365acd123940a49cc456690670e7794966d2d8ce
onehao111 <onehao111@gmail.com>
2011-06-24 09:53:04 +0000

    update TestHashcode.java

--- javaAdvance/src/com/onehao/collection/map/TestHashCode.java
+++ javaAdvance/src/com/onehao/collection/map/TestHashCode.java
@@ -24,25 +24,11 @@ public class TestHashCode
 		Class clazz = Class.forName("java.util.HashMap");
 		Class innerClazz = Class.forName("java.util.HashMap$Entry");
 		
-		Method put = clazz.getDeclaredMethod("put", new Class[]{Object.class, Object.class});
+//		Method put = clazz.getDeclaredMethod("put", new Class[]{Object.class, Object.class});
 		
-		Method[] methods = clazz.getMethods();
-		for(Method m : methods)
-		{
-			System.out.println(m.getName());
-		}
-		
-		Map.Entry m = (Map.Entry)innerClazz.newInstance();
-		
-		put.invoke(m, new Object[]{2, 3});
-		put.invoke(m, new Object[]{2, 4});
-		
-		Method getValue = innerClazz.getMethod("getValue", new Class[]{});
-		
-		getValue.setAccessible(true);
-		getValue.invoke(m, null);
-		
-		Class[] innerClass = clazz.getClasses();
+		Field f = innerClazz.getDeclaredField("next");
+		Map.Entry entry = (Map.Entry)innerClazz.newInstance();
+		entry.getValue();
 		
 	}
 }
