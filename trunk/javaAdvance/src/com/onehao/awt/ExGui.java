5f0fd0a145d1aab50b8e603de7659b10d85c9add
onehao111@gmail.com <onehao111@gmail.com@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-06-26 13:56:11 +0000

    Complete - task : Java SE 076 AWT详解

---
+++ trunk/javaAdvance/src/com/onehao/awt/ExGui.java
@@ -0,0 +1,38 @@
+package com.onehao.awt;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+
+public class ExGui
+{
+	private Frame frame;
+	
+	private Button button1;
+	
+	private Button button2;
+	
+	public void go()
+	{
+		frame = new Frame("gui example");
+		
+		frame.setLayout(new FlowLayout());
+		
+		button1 = new Button("press me");
+		button2 = new Button("Don't press me");
+		
+		frame.add(button1);
+		frame.add(button2);
+		
+		frame.pack();
+		
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		ExGui window = new ExGui();
+		
+		window.go();
+	}
+}
---
+++ trunk/javaAdvance/src/com/onehao/awt/MyFrame.java
@@ -0,0 +1,23 @@
+package com.onehao.awt;
+
+import java.awt.Color;
+import java.awt.Frame;
+
+public class MyFrame extends Frame
+{
+	public MyFrame(String title)
+	{
+		super(title);
+	}
+	
+	public static void main(String[] args)
+	{
+		MyFrame frame = new MyFrame("First GUI app");
+		
+		frame.setSize(500, 500);
+		
+		frame.setBackground(Color.cyan);
+		
+		frame.setVisible(true);
+	}
+}
