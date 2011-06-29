095dc0109001f63a38592d1d6d010712a88f846d
onehao111 <onehao111@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-06-29 14:04:30 +0000

    Incomplete - task : Java SE 086 Swing 内部类

---
+++ javaAdvance/src/com/onehao/innerclass/SwingTest.java
@@ -0,0 +1,51 @@
+package com.onehao.innerclass;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class SwingTest
+{
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("JFrame");
+		
+		JButton button = new JButton("click me");
+		
+		button.addActionListener(new ActionListener()
+			{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println("welcome");
+			}
+			});
+		frame.setSize(new Dimension(200, 200));
+		
+		frame.getContentPane().add(button);
+		
+		frame.addWindowListener(new WindowAdapter(){
+			@Override
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		
+		frame.setVisible(true);
+	}
+}
+
+//class MyListener implements ActionListener
+//{
+//	@Override
+//	public void actionPerformed(ActionEvent e)
+//	{
+//		System.out.println("hello world");
+//	}
+//}
