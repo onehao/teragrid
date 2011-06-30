be5bf95d35a9057b3efb2bd42fcf0e227da4d161
onehao111@gmail.com <onehao111@gmail.com@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-06-30 05:05:01 +0000

    Complete - task : Java SE 088 递归深度详解_2

---
+++ trunk/javaAdvance/src/com/onehao/io/FileTest9.java
@@ -0,0 +1,29 @@
+package com.onehao.io;
+
+import java.io.File;
+
+public class FileTest9
+{
+	public static void deleteAll(File file)
+	{
+		if(file.isFile() || file.list().length == 0)
+		{
+			file.delete();
+		}
+		else
+		{
+			File[] files = file.listFiles();
+			
+			for(File f : files)
+			{
+				deleteAll(f);
+				f.delete();
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		deleteAll(new File("..."));
+	}
+}
