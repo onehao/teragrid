c004cba2dccbc79b0ed9595a18e2bedb2851c048
onehao111 <onehao111@gmail.com>
2011-07-02 05:10:06 +0000

    Complete - task : Java SE 094 Java字符流详解

--- javaAdvance/src/com/onehao/io/decorator/Client.java
+++ javaAdvance/src/com/onehao/io/decorator/Client.java
@@ -10,9 +10,22 @@ public class Client
 		//过滤流
 		Component component2 = new ConcreteDecorator1(component);
 		
+		component2.doSomething();
+		
+		System.out.println(" ***************** ");
+		
 		//过滤流
 		Component component3 = new ConcreteDecorator2(component2);
 		
 		component3.doSomething();
+		
+		
+		System.out.println(" ----------------- ");
+		
+		Component component4 = new ConcreteDecorator1(
+				new ConcreteDecorator2(
+						new ConcreteComponent()));
+		
+		component4.doSomething();
 	}
 }
---
+++ javaAdvance/src/com/onehao/io3/StreamTest2.java
@@ -0,0 +1,23 @@
+package com.onehao.io3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class StreamTest2
+{
+	public static void main(String[] args) throws Exception
+	{
+		InputStreamReader isr = new InputStreamReader(System.in);
+		
+		BufferedReader br = new BufferedReader(isr);
+		
+		String str;
+		
+		while(null != (str = br.readLine()))
+		{
+			System.out.println(str);
+		}
+		
+		System.out.println();
+	}
+}
