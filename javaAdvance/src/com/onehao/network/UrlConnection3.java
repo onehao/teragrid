d8090a2e4bc3f9a17ea26c368cf264beaed0e1c4
onehao111 <onehao111@gmail.com>
2011-07-12 13:33:49 +0000

    Complete - task : Java SE 126 深入详解JVM之类加载器深度剖析、根、扩展及系统类加载器

--- javaAdvance/src/com/onehao/network/UrlConnection3.java
+++ javaAdvance/src/com/onehao/network/UrlConnection3.java
@@ -1,8 +1,12 @@
 package com.onehao.network;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 public class UrlConnection3
 {
@@ -10,15 +14,41 @@ public class UrlConnection3
 	{
 		URL url = new URL("http://www.sohu.com");
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(),"GBK"));
+		InputStream is = url.openStream();
+		
+		InputStreamReader isr = new InputStreamReader(is, Charset.forName("GB18030"));
+		
+		FileOutputStream fos = new FileOutputStream("gen/sohu2.html");
+		
+		OutputStreamWriter osw = new OutputStreamWriter(fos);
+		
+		char[] b = new char[2048];
+		
+		int temp;
+		
+		while(-1 != (temp = isr.read(b, 0, b.length)))
+		{
+			osw.write(b);
+		}
+		
+		osw.close();
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(),"GB18030"));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 		
 		String line = null;
 		
-		while(null != (line = br.readLine()))
-		{
-			System.out.println(line);
-		}
-		
-		br.close();
+//		while(null != (line = br.readLine()))
+//		{
+//			byte[] b = line.getBytes("GB18030");
+////			System.out.println(b.toString());
+//		}
+//		
+//		br.close();
+//		
+//		Charset c = Charset.forName("GB18030");
+//		String s = new String("ÖÐÎÄ".getBytes(), c);
+//		System.out.println(s);
 	}
 }
