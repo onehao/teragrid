ec6e2e5b9ab44a72f0e4164863da6f151d89e197
onehao111 <onehao111@5987a6a4-df0d-865c-bea1-9ffa38a98c27>
2011-07-15 12:34:12 +0000

    Complete - task : Java SE 109 TCP连接建立的一般过程与代码详解

---
+++ javaAdvance/src/com/onehao/network/socket/TcpClient.java
@@ -0,0 +1,34 @@
+package com.onehao.network.socket;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TcpClient
+{
+	public static void main(String[] args) throws Exception, Exception
+	{
+		Socket socket = new Socket("127.0.0.1", 5000);
+
+		OutputStream os = socket.getOutputStream();
+
+		os.write("hello world".getBytes());
+		
+		InputStream is = socket.getInputStream();
+
+		byte[] buffer = new byte[200];
+		
+		int length = 0;
+
+		while (-1 != (length = is.read(buffer, 0, buffer.length)))
+		{
+			String str = new String(buffer, 0, length);
+
+			System.out.println(str);
+		}
+		
+		is.close();
+		os.close();
+		socket.close();
+	}
+}
